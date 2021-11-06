ab0951b23f186b0f0c3f0ddf9588023fd047125d
Julian Grisales <jgrisales@gmail.com>
2021-11-06 21:14:23 +0100

    CRUD Delete a Post

--- main.py
+++ main.py
@@ -22,6 +22,11 @@ def find_post(id):
         if post['id'] == id:
             return post
 
+def find_index_post(id):
+    for index, post in enumerate(my_posts):
+        if post['id'] == id:
+            return index
+
 
 @app.get("/")
 def root():
@@ -51,3 +56,12 @@ def get_post(id: int):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f"post with id: {id} was not found")
     return {"data": post}
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with {id} does not exists")
+    my_posts.pop(index)
+    return {"message": "Post was succesfuly deleted"}
