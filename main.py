aa04da874f51b6f79340d66f6c72d69161d22158
Julian Grisales <jgrisales@gmail.com>
2021-11-06 21:00:33 +0100

    CRUD Read a Post

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
@@ -33,8 +33,8 @@ def get_posts():
     return {"data": my_posts}
 
 
-@app.post("/posts")
-def create_posts(post: Post):
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def create_post(post: Post):
     post_dict = post.dict()
     post_dict['id'] = randrange(0, 1000000)
     my_posts.append(post_dict)
@@ -47,4 +47,7 @@ def create_posts(post: Post):
 @app.get("/posts/{id}")
 def get_post(id: int):
     post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"post with id: {id} was not found")
     return {"data": post}
