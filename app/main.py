d7900aee4eeda8610e8f23e5ec1ce5d5a700a292
Julian Grisales <jgrisales@gmail.com>
2021-11-07 17:49:53 +0100

    ORM for PostgreSQL with SQLAlchemy

--- app/main.py
+++ app/main.py
@@ -1,12 +1,19 @@
-from fastapi import FastAPI, status, HTTPException
+from fastapi import FastAPI, status, HTTPException, Depends
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
+from sqlalchemy.orm import Session
+from . import models
+from .database import engine, get_db
+
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
 
 class Post(BaseModel):
     title: str
@@ -45,6 +52,11 @@ def find_index_post(id):
 @app.get("/")
 def root():
     return {"message": "Hello World"}
+
+
+@app.get("/sqlalchemy")
+def test_posts(db: Session = Depends(get_db)):
+    return {"status": "OK"}
 
 
 @app.get("/posts")
