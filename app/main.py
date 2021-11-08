aa7c8d9740852038d852d435f034fe958a2a2297
Julian Grisales <jgrisales@gmail.com>
2021-11-08 22:10:24 +0100

    User hashing password

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
 from sqlalchemy.orm import Session
-from . import models, schemas
+from . import models, schemas, utils
 from .database import engine, get_db
 
 
@@ -93,6 +93,11 @@ def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)
 
 @app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+
+    # hash the password
+    hashed_password = utils.hash(user.password)
+    user.password = hashed_password
+
     # **post.dict() unpack a dict like params > {'title': 'Hallo'} to (title='Hallo')
     new_user = models.User(**user.dict())
     db.add(new_user)
